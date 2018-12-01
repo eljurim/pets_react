8d99106dc96dc889648c308f5b3b0a9bc6c07311
Edwin <edwin_020193@hotmail.com>
2018-11-30 21:07:43 -0600

    bootstrap

---
+++ pets/src/components/formPet/formPet.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import './formPet.css';
+
+class FormPet extends Component{
+    render(){
+
+        return(
+            <form>
+                <div>Pets Form</div>
+            </form>
+        );
+    }
+}
+ 
+
+export default FormPet;
