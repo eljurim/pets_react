479a0426597f2dca85a03ae54647667f008d30dc
Manuel Eljuri <manueleljuri@gmail.com>
2018-11-30 21:55:11 -0600

    decostruccion obj

--- pets/src/App.js
+++ pets/src/App.js
@@ -3,17 +3,25 @@ import React, { Component } from 'react';
 import FormPet from './components/formPet/formPet';
 import ListPets from './components/listPets/listPets'
 import './App.css';
-import './js/bootstrap'
 
 class App extends Component {
   render() {
+    const testObj = {
+      "breed": "Boxer",
+      "checkIn": "2018-09-21",
+      "checkOut": "2018-12-12",
+      "diagnosis": "TBD",
+      "name": "Cheeka",
+      "owner": "Manuel",
+      "symptoms": "Hongos en la piel"
+  }
+
     return (
         <div>
             <FormPet></FormPet>
             <br></br>
-            <ListPets></ListPets>
+            <ListPets obj={testObj}></ListPets>
         </div>
-        <script></script>
     );
   }
 }
