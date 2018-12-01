6cde718b022779b91554bbe6c58c1eb1ab93a14f
Edwin <edwin_020193@hotmail.com>
2018-11-30 21:29:41 -0600

    add Boostratp

--- pets/src/App.js
+++ pets/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import FormPet from './components/formPet/formPet';
 import './App.css';
+import './js/bootstrap'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
         <div>
             <FormPet></FormPet>
         </div>
+        <script></script>
     );
   }
 }
