40e28000ab97fd7164a1798085983bb72d8d0e40
Manuel Eljuri <manueleljuri@gmail.com>
2018-12-03 22:14:18 -0600

    listo inputs

--- pets/src/components/formPet/formPet.js
+++ pets/src/components/formPet/formPet.js
@@ -1,46 +1,50 @@
 import React,{Component} from 'react';
 import './formPet.css';
 
-class FormPet extends Component{
-    render(){
 
+class FormPet extends Component{
+    constructor(props) {
+        super(props)
+        this.handleForm = this.handleForm.bind(this)
+    }
+
+
+    handleForm(e) {
+        e.preventDefault()
+        let name = document.getElementsByClassName('name')[0].value
+        let breed = document.getElementsByClassName('breed')[0].value
+        let owner = document.getElementsByClassName('owner')[0].value
+        let checkin = document.getElementsByClassName('checkin')[0].value
+        let newEntryData = { name, breed, owner, checkin }
+        console.log(newEntryData)
+    }
+
+    render(){
         return(
             <form>
-                <div>Pets Form</div>
-                            <table class="table">
-                        <thead class="thead-dark">
-                            <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            </tr>
-                            <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                            </tr>
-                            <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            </tr>
-                        </tbody>
-            </table>
+                <div className='titulo'>Pets Form</div>
+                    <div className='principal'>
+                        <div className='contenedor'>
+                            <label className='label'>Nombre de la Mascota: </label>
+                            <input className='input name' placeholder='Ingresa el nombre'></input>
+                        </div>
+                        <div className='contenedor'>
+                            <label className='label'>Raza: </label>
+                            <input className='input breed' placeholder='Ingresa la raza'></input>
+                        </div>
+                        <div className='contenedor'>
+                            <label className='label'>Dueño: </label>
+                            <input className='input owner' placeholder='Ingresa nombre del dueño'></input>
+                        </div>
+                        <div className='contenedor'>
+                            <label className='label'>Checkin: </label>
+                            <input className='input checkin' type='date' placeholder='Ingresa fecha de ingreso'></input>
+                        </div>
+                        <div className='contenedor'><button className='boton' type='submit' onClick={this.handleForm}>Guardar</button></div>
+                    </div>                
             </form>
         );
     }
 }
  
-
 export default FormPet;
